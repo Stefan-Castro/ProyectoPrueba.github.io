630b0eb31a96c316b16b0ac3012f5c054370b1b3
AlvaroChiriguayaV <96268905+AlvaroChiriguayaV@users.noreply.github.com>
2022-01-16 00:13:41 -0500

    Update JavaScriptA.js

--- assets/js/JavaScriptA.js
+++ assets/js/JavaScriptA.js
@@ -1,4 +1,45 @@
+//Imagenes principales 
+function agrandarImagen1() {
+    document.getElementById("imagen1").style.width = "33%";
+    document.getElementById("imagen1").style.height = "320px";
+    document.getElementById("imagen1").style.marginLeft = "1%";
+    document.getElementById("imagen2").style.marginLeft = "1%";
+}
 
+function normalImagen1() {
+    document.getElementById("imagen1").style.width = "30%";
+    document.getElementById("imagen1").style.height = "300px";
+    document.getElementById("imagen1").style.marginLeft = "2%";
+    document.getElementById("imagen2").style.marginLeft = "2%";
+}
+
+function agrandarImagen2() {
+    document.getElementById("imagen2").style.width = "33%";
+    document.getElementById("imagen2").style.height = "320px";
+    document.getElementById("imagen2").style.marginLeft = "1%";
+    document.getElementById("imagen3").style.marginLeft = "1%";
+}
+
+function normalImagen2() {
+    document.getElementById("imagen2").style.width = "30%";
+    document.getElementById("imagen2").style.height = "300px";
+    document.getElementById("imagen2").style.marginLeft = "2%";
+    document.getElementById("imagen3").style.marginLeft = "2%";
+}
+
+function agrandarImagen3() {
+    document.getElementById("imagen3").style.width = "33%";
+    document.getElementById("imagen3").style.height = "320px";
+    document.getElementById("imagen3").style.marginLeft = "1%";
+}
+
+function normalImagen3() {
+    document.getElementById("imagen3").style.width = "30%";
+    document.getElementById("imagen3").style.height = "300px";
+    document.getElementById("imagen3").style.marginLeft = "2%";
+}
+
+//Efecto de imagenes secundarias
 window.addEventListener('scroll', function () {
 
     var imagen = document.querySelectorAll('.fade_up');
@@ -13,24 +54,48 @@ window.addEventListener('scroll', function () {
             imagen[i].classList.remove('show_img');
         }
     }
-
-
 })
 
 //Botón para volver hacia arriba
-const toTop = document.querySelector(".to-top");
-window.addEventListener('scroll'), () => {
-   if(window.pageYOffset >100){
-    toTop.classList.add("active");
-   }else{
-    toTop.classList.remove("active");
-   }
-  
+const botonTop = document.querySelector(".boton-top");
 
+botonTop.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+});
 
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 500) {
+        botonTop.style.right = -100 + "px";
+    } else {
+        botonTop.style.right = 5 + "px";
+    }
+});
+
+//Acción del botón Reservar en el formulario
+function validarReserva() {
+    resultado = false;
+    resultado = enviar();
+    if (resultado === true) {
+        alert("Su reservación ha sido enviada con éxito");
+    }
+    return resultado;
 }
 
-        
+function enviar() {
+    var mensaje;
+    var opciones = confirm("Está seguro de realizar su reserva!");
+    if (opciones == true) {
+        valor = true;
+    } else {
+        valor = false;
+    }
+    document.getElementById("botonReservaP").innerHTML = mensaje;
+    return valor;
+}
+
+
+
+
 
 
 
