9772dc3f6639baf51032b56f6997df9659e1dc4b
AlvaroChiriguayaV <96268905+AlvaroChiriguayaV@users.noreply.github.com>
2022-01-16 12:12:30 -0500

    Update JavaScriptA.js

--- assets/js/JavaScriptA.js
+++ assets/js/JavaScriptA.js
@@ -1,45 +1,57 @@
-//Imagenes principales 
-function agrandarImagen1() {
-    document.getElementById("imagen1").style.width = "33%";
-    document.getElementById("imagen1").style.height = "320px";
+//DOM
+//Slider de Imagenes Principales
+function agrandarImagen1(){
+    document.getElementById("imagen1").style.width="33%";
+    document.getElementById("imagen1").style.height="320px";
     document.getElementById("imagen1").style.marginLeft = "1%";
     document.getElementById("imagen2").style.marginLeft = "1%";
 }
 
 function normalImagen1() {
-    document.getElementById("imagen1").style.width = "30%";
-    document.getElementById("imagen1").style.height = "300px";
+    document.getElementById("imagen1").style.width="30%";
+    document.getElementById("imagen1").style.height="300px";
     document.getElementById("imagen1").style.marginLeft = "2%";
     document.getElementById("imagen2").style.marginLeft = "2%";
 }
 
-function agrandarImagen2() {
-    document.getElementById("imagen2").style.width = "33%";
-    document.getElementById("imagen2").style.height = "320px";
+function agrandarImagen2(){
+    document.getElementById("imagen2").style.width="33%";
+    document.getElementById("imagen2").style.height="320px";
     document.getElementById("imagen2").style.marginLeft = "1%";
     document.getElementById("imagen3").style.marginLeft = "1%";
 }
 
 function normalImagen2() {
-    document.getElementById("imagen2").style.width = "30%";
-    document.getElementById("imagen2").style.height = "300px";
+    document.getElementById("imagen2").style.width="30%";
+    document.getElementById("imagen2").style.height="300px";
     document.getElementById("imagen2").style.marginLeft = "2%";
     document.getElementById("imagen3").style.marginLeft = "2%";
 }
 
-function agrandarImagen3() {
-    document.getElementById("imagen3").style.width = "33%";
-    document.getElementById("imagen3").style.height = "320px";
+function agrandarImagen3(){
+    document.getElementById("imagen3").style.width="33%";
+    document.getElementById("imagen3").style.height="320px";
     document.getElementById("imagen3").style.marginLeft = "1%";
 }
 
 function normalImagen3() {
-    document.getElementById("imagen3").style.width = "30%";
-    document.getElementById("imagen3").style.height = "300px";
+    document.getElementById("imagen3").style.width="30%";
+    document.getElementById("imagen3").style.height="300px";
     document.getElementById("imagen3").style.marginLeft = "2%";
 }
 
-//Efecto de imagenes secundarias
+function textoCambiado() {
+    var texto = document.getElementsByClassName("centro");
+    texto[0].innerHTML = "Estimado Cliente disfrute de nuestros atractivos paquetes promocionales.<br> Nunca ha habido un mejor momento "+
+    "para disfrutar de nuestro grandioso Lux Hotel.";
+}
+
+function textoNormal() {
+    var texto = document.getElementsByClassName("centro");
+    texto[0].innerHTML = " Guayaquil, Ecuador es un gran destino para vacacionar. <br> Lux Hotel es la mejor elección con grandes paquetes y promociones para mejorar su experiencia.";
+}
+//DOM
+//Efecto de fade up para las imagenes secundarias
 window.addEventListener('scroll', function () {
 
     var imagen = document.querySelectorAll('.fade_up');
@@ -56,6 +68,7 @@ window.addEventListener('scroll', function () {
     }
 })
 
+//DOM
 //Botón para volver hacia arriba
 const botonTop = document.querySelector(".boton-top");
 
@@ -64,24 +77,25 @@ botonTop.addEventListener("click", () => {
 });
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY < 500) {
+    if(window.scrollY < 500){
         botonTop.style.right = -100 + "px";
-    } else {
+    }else{
         botonTop.style.right = 5 + "px";
     }
 });
 
-//Acción del botón Reservar en el formulario
-function validarReserva() {
+//DOM
+//Acción del Botón Reservar fomulario
+function validarReserva(){
     resultado = false;
     resultado = enviar();
-    if (resultado === true) {
+    if(resultado===true){
         alert("Su reservación ha sido enviada con éxito");
     }
     return resultado;
 }
 
-function enviar() {
+function enviar(){
     var mensaje;
     var opciones = confirm("Está seguro de realizar su reserva!");
     if (opciones == true) {
@@ -92,8 +106,4 @@ function enviar() {
     document.getElementById("botonReservaP").innerHTML = mensaje;
     return valor;
 }
-
-
-
-
-
+        
