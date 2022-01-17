367662f8a54914a5a873d66fb1589c436922881e
AlvaroChiriguayaV <96268905+AlvaroChiriguayaV@users.noreply.github.com>
2022-01-16 22:18:02 -0500

    Update JavaScriptA.js

--- assets/js/JavaScriptA.js
+++ assets/js/JavaScriptA.js
@@ -87,12 +87,123 @@ window.addEventListener("scroll", () => {
 //DOM
 //Acción del Botón Reservar fomulario
 function validarReserva(){
-    resultado = false;
-    resultado = enviar();
-    if(resultado===true){
-        alert("Su reservación ha sido enviada con éxito");
+    resultado = true;
+    var txtNombres = document.getElementById("nombres");
+    var txtApellidos = document.getElementById("apellidos");
+    var txtTelefono = document.getElementById("telefono");
+    var txtemail = document.getElementById("correo");
+    var txtFecha = document.getElementById("fecha");
+    var txtHora = document.getElementById("hora");
+
+    limpiarMensajes();
+    var valNombre = nombres(txtNombres);
+    var valApellido = nombres(txtApellidos);
+    var valTelefono = telefono(txtTelefono);
+    var valCorreo = email(txtemail);
+    var valFecha = fecha(txtFecha);
+    var valHora = hora(txtHora);
+
+    if(valNombre === false || valApellido === false || valCorreo === false || valTelefono === false ||
+        valFecha === false || valHora === false){
+        resultado = false;
     }
+
+    if(resultado===true){
+        resultado = enviar();
+        if(resultado===true){
+            alert("Su reservación ha sido enviada con éxito");
+        }
+    }
+
     return resultado;
+}
+
+function nombres(txtVariable){
+    var letra = /^[a-z ,.'-]+$/i;// letras y espacio ///^[A-Z]+$/i;// solo letras
+    if (txtVariable.value === '') {
+        mensaje("『 Este campo es requerido 』", txtVariable);
+        return false;
+    } else if (!letra.test(txtVariable.value)) {
+        mensaje("『 Este campo solo debe contener letras 』", txtVariable);
+        return false;
+    } else if (txtVariable.value.length > 20) {
+        mensaje("『 Este campo debe tener maximo 20 caracteres 』", txtVariable);
+        return false;
+    }
+}
+
+function email(txtemail) {
+    var correo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (txtemail.value === "") {
+        mensaje("『 Email es requerido 』", txtemail);
+        return false;
+    } else if (!correo.test(txtemail.value)) {
+        mensaje("『 Email no es correcto 』", txtemail);
+        return false;
+    }
+}
+
+function telefono(txtTelefono) {
+    var telefonoreg = /^[0-9]{10}$/g; // para validar datos que deban tener 10 numeros
+    if (txtTelefono.value === "") {
+        mensaje("『 Teléfono es requerido 』", txtTelefono);
+        return false;
+    } else if (!telefonoreg.test(txtTelefono.value)) {
+        mensaje("『 Teléfono debe ser de 10 digitos 』", txtTelefono);
+        return false;
+    }
+}
+
+function fecha(txtFecha) {
+    var dato = txtFecha.value;
+    var fechaN = new Date(dato);
+    var anioN = fechaN.getFullYear();
+    
+    var fechaActual = new Date(); //fecha actual
+    var anioA = fechaActual.getFullYear();
+
+    if(dato === ""){
+        mensaje("『 Fecha es requerida 』",txtFecha);
+        return false;
+    }else if(fechaN < fechaActual){
+        mensaje("『 Fecha debe ser superior a la actual 』",txtFecha);
+        return false;
+    }else if(anioN > (anioA+2)){
+        mensaje("『 No puede asignar una reserva mayor a 2 años 』",txtFecha);
+        return false;
+    }
+}
+
+function hora(txtHora) {
+    var dato = txtHora.value;
+    var separador = dato.split(":");
+    horaN = parseInt(separador[0]);
+    minutoN = parseInt(separador[1]);
+
+    if(dato === ""){
+        mensaje("『 Hora es requerida 』",txtHora);
+        return false;
+    }
+}
+
+function mensaje(cadenaMensaje, elemento) {
+    elemento.focus();
+    var nodoPadre = elemento.parentNode;
+    
+    var nodoMensaje = document.createElement("span");
+    nodoMensaje.innerHTML = cadenaMensaje;
+    nodoMensaje.style.color = "red";
+    nodoMensaje.display = "block";
+    nodoMensaje.setAttribute("class", "mensaje");
+    
+    nodoPadre.appendChild(nodoMensaje);
+}
+
+function limpiarMensajes() {
+    var mensajes = document.querySelectorAll(".mensaje");
+    for (let i = 0; i < mensajes.length; i++) {
+        mensajes[i].remove();// remueve o elimina un elemento de mi doc html
+    }
 }
 
 function enviar(){
@@ -106,8 +217,4 @@ function enviar(){
     document.getElementById("botonReservaP").innerHTML = mensaje;
     return valor;
 }
-        
-
-
-
 
